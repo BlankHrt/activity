879e00560814cec609246a7f043cf6496bfd5734
kj <310784976@qq.com>
2017-09-19 19:31:58 +0800

    'form'

--- src/app/activity/activityAdd.component.ts
+++ src/app/activity/activityAdd.component.ts
@@ -4,7 +4,7 @@
 
 import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, OnInit, Renderer, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder,  } from '@angular/forms';
 import { Common } from '../shared/Common';
 import { ActivityService } from './activity.service';
 import { Location } from '@angular/common';
@@ -23,18 +23,21 @@ export class ActivityAddComponent implements OnInit, AfterViewInit, OnDestroy {
   HttpUrl = Common.HttpUrl;
   ActivityUrl = Common.ActivityUpload;
   activityType;
-  form: FormGroup;
-  title: FormControl;
   content;
-  address: FormControl;
   startTime;
   endTime;
-  publish_user_contact: FormControl;
   elementRef: ElementRef;
   imageList = [];
   showSpinner = false;
   label: String = '';
   labelList: Array<any> = [];
+
+  // form
+  activity = {
+    title: null,
+    address: null,
+    publishUserContact: null
+  };
 
   // unsubscribe :forms,router,render service,Infinite Observables ,Redux Store
   // don't unsubscribe:Async pipe,@HostListener ,Finite Observable
@@ -58,7 +61,7 @@ export class ActivityAddComponent implements OnInit, AfterViewInit, OnDestroy {
   };
   ActivityUpload = Common.ActivityUpload;
   constructor( @Inject(ElementRef) elementRef: ElementRef,
-    private store: Store<any>, private location: Location, private formBuilder: FormBuilder,
+    private store: Store<any>, private location: Location,
     private activityService: ActivityService, private router: Router,
     private route: ActivatedRoute, private renderer: Renderer) {
     this.elementRef = elementRef;
@@ -114,20 +117,9 @@ export class ActivityAddComponent implements OnInit, AfterViewInit, OnDestroy {
     this.storeSubscribe = this.store.select('user').subscribe(data => {
       this.user = data;
     });
-    this.title = new FormControl('', [Validators.required, Validators.maxLength(30)]);
-    this.address = new FormControl('', [Validators.required, Validators.maxLength(50)]);
-    this.publish_user_contact = new FormControl('', [Validators.required]);
-
-    this.form = this.formBuilder.group({
-      title: this.title,
-      address: this.address,
-      publish_user_contact: this.publish_user_contact,
-    });
   }
 
   imageUploaded(e) {
-    this.renderer.setElementProperty(this.elementRef.nativeElement.querySelector('.clear span'), 'innerHTML', '清除所有');
-    this.renderer.setElementStyle(this.elementRef.nativeElement.querySelector('.clear'), 'background-color', '#eb7350');
     this.imageList.push(
       e.serverResponse.text()
     );
@@ -143,26 +135,24 @@ export class ActivityAddComponent implements OnInit, AfterViewInit, OnDestroy {
     console.log(this.imageList);
   }
 
-  lableChange(e) {
-    this.labelList = e.split(' ').filter(a => !!a);
-  }
-
   commit() {
-    this.showSpinner = true;
-    this.renderer.setElementAttribute(this.commitButton.nativeElement, 'disabled', 'true');
-    if (this.user.user.id) {
-      this.schoolId = this.user.user.schoolId;
-      for (let i = 0; i < this.imageList.length; i++) {
-        this.imageList[i] = '\"' + this.imageList[i] + '\"';
-      }
-      this.content = $('#summernote').summernote('code');
-      this.activityService.insert(this.form.value, this.startTime, this.endTime,
-        this.content, this.user.user.id, this.activityType, this.schoolId, [this.imageList]).subscribe(data => {
-          this.router.navigate(['../list'], { relativeTo: this.route });
-        });
-    } else {
-      alert('登陆超时，请重新登录');
-    }
+      if (this.activity.title && this.activity.address && this.activity.publishUserContact) {
+        this.showSpinner = true;
+        this.renderer.setElementAttribute(this.commitButton.nativeElement, 'disabled', 'true');
+        if (this.user.user.id) {
+          this.schoolId = this.user.user.schoolId;
+          for (let i = 0; i < this.imageList.length; i++) {
+            this.imageList[i] = '\"' + this.imageList[i] + '\"';
+          }
+          this.content = $('#summernote').summernote('code');
+          this.activityService.insert(this.activity, this.startTime, this.endTime,
+            this.content, this.user.user.id, this.activityType, this.schoolId, [this.imageList]).subscribe(data => {
+            this.router.navigate(['../list'], {relativeTo: this.route});
+          });
+        } else {
+          alert('登陆超时，请重新登录');
+        }
+      }//if
   }
 
   back() {
