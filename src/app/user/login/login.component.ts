7eb0dbf1b0b7b3e1b07a1525e8fd983ebc45eed6
liuqinghua <759345232@qq.com>
2017-09-26 18:56:02 +0800

    cookie expire

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -45,7 +45,7 @@ export class LoginComponent implements OnInit {
               user: data.user
             }
           });
-          this.cookieService.put('token', data.token, { expires: '6000' });
+          this.cookieService.put('token', data.token, { expires: (new Date(Date.now() + 2592000 * 1000)).toString() });
           this.showError = false;
           this.store.select('router').subscribe((x: any) => {
             this.router.navigate([x.url]);
