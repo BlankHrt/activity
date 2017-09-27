4a94b4e867fa27e47ef6853d55b12d65c0b06cc2
liuqinghua <759345232@qq.com>
2017-09-27 21:22:27 +0800

    wx-server4

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -93,18 +93,17 @@ export class ActivityComponent implements OnInit, OnDestroy {
     this.store.select('wx').subscribe(data => {
       if (data.JsapiTicket) {
         this.newUrl = this.url + '/activity/list';
-        this.activityService.signature(data.JsapiTicket, data.nonceStr, data.timestamp,
-          encodeURIComponent(this.newUrl)).subscribe(data2 => {
-            wx.config({
-              debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-              appId: 'wx3b6fe19df1feedfa', // 必填，公众号的唯一标识
-              timestamp: data2.timestamp, // 必填，生成签名的时间戳
-              nonceStr: data2.nonceStr, // 必填，生成签名的随机串
-              signature: data2.signature, // 必填，签名，见附录1
-              // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-              jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone']
-            });
+        this.activityService.signature(data.JsapiTicket, data.nonceStr, data.timestamp, this.newUrl).subscribe(data2 => {
+          wx.config({
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: 'wx3b6fe19df1feedfa', // 必填，公众号的唯一标识
+            timestamp: data2.timestamp, // 必填，生成签名的时间戳
+            nonceStr: data2.nonceStr, // 必填，生成签名的随机串
+            signature: data2.signature, // 必填，签名，见附录1
+            // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone']
           });
+        });
       }
     });
 
