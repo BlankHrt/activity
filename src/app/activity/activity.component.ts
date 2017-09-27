52acf86f8b7088a58ebc470c3ba48bc6a4c7e3f1
liuqinghua <759345232@qq.com>
2017-09-27 22:37:51 +0800

    wx-server10-success

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -95,7 +95,7 @@ export class ActivityComponent implements OnInit, OnDestroy {
         this.newUrl = this.url + '/activity/list';
         this.activityService.signature(data.JsapiTicket, data.nonceStr, data.timestamp, this.newUrl).subscribe(data2 => {
           wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+           // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: 'wx3b6fe19df1feedfa', // 必填，公众号的唯一标识
             timestamp: data2.timestamp, // 必填，生成签名的时间戳
             nonceStr: data2.nonceStr, // 必填，生成签名的随机串
@@ -103,9 +103,6 @@ export class ActivityComponent implements OnInit, OnDestroy {
             // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone']
           });
-          console.log('后台返回');
-          console.log(data2);
-          this.newUrl = data2.url;
         });
       }
     });
--- src/app/activity/activityDetail.component.ts
+++ src/app/activity/activityDetail.component.ts
@@ -145,7 +145,7 @@ export class ActivityDetailComponent implements OnInit, AfterViewInit, OnDestroy
             this.activityService.signature(data.JsapiTicket, data.nonceStr, data.timestamp,
               encodeURIComponent(this.newUrl)).subscribe(data2 => {
                 wx.config({
-                  debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                  // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                   appId: 'wx3b6fe19df1feedfa', // 必填，公众号的唯一标识
                   timestamp: data2.timestamp, // 必填，生成签名的时间戳
                   nonceStr: data2.nonceStr, // 必填，生成签名的随机串
