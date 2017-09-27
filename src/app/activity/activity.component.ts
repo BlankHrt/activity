ac4325171c96c13e103c23890f370522ba461aaa
liuqinghua <759345232@qq.com>
2017-09-27 21:08:56 +0800

    wx-server4

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -93,24 +93,25 @@ export class ActivityComponent implements OnInit, OnDestroy {
     this.store.select('wx').subscribe(data => {
       if (data.JsapiTicket) {
         this.newUrl = this.url + '/activity/list';
-        this.activityService.signature(data.JsapiTicket, data.nonceStr, data.timestamp, this.newUrl).subscribe(data2 => {
-          wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: 'wx3b6fe19df1feedfa', // 必填，公众号的唯一标识
-            timestamp: data2.timestamp, // 必填，生成签名的时间戳
-            nonceStr: data2.nonceStr, // 必填，生成签名的随机串
-            signature: data2.signature, // 必填，签名，见附录1
-            // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-            jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone']
+        this.activityService.signature(data.JsapiTicket, data.nonceStr, data.timestamp,
+          encodeURIComponent(this.newUrl)).subscribe(data2 => {
+            wx.config({
+              debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+              appId: 'wx3b6fe19df1feedfa', // 必填，公众号的唯一标识
+              timestamp: data2.timestamp, // 必填，生成签名的时间戳
+              nonceStr: data2.nonceStr, // 必填，生成签名的随机串
+              signature: data2.signature, // 必填，签名，见附录1
+              // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+              jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone']
+            });
           });
-        });
       }
     });
 
     wx.onMenuShareAppMessage({
       title: '大学生活动列表', // 分享标题
       desc: '这里有丰富多彩的活动，邀请您一起参与', // 分享描述
-      link: encodeURIComponent(this.newUrl), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      link: this.newUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
       imgUrl: '../../assets/img/logo.jpg', // 分享图标
       type: '', // 分享类型,music、video或link，不填默认为link
       dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
