6f89832516b21f71d20b8d4af910446726999e40
liuqinghua <759345232@qq.com>
2017-09-27 22:16:27 +0800

    wx-server8

--- src/app/activity/activityDetail.component.ts
+++ src/app/activity/activityDetail.component.ts
@@ -99,6 +99,23 @@ export class ActivityDetailComponent implements OnInit, AfterViewInit, OnDestroy
             });
           }
           this.imageList = list;
+          wx.onMenuShareAppMessage({
+            title: this.activity.title, // 分享标题
+            desc: $('#summernote')[0].innerText, // 分享描述
+            link: this.newUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: this.imageList[0].medium, // 分享图标
+            type: '', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+              this.snackBar.open('分享成功');
+              setTimeout(() => {
+                this.snackBar.dismiss();
+              }, 1500);
+            },
+            cancel: function () {
+              // 用户取消分享后执行的回调函数
+            }
+          });
         });
 
         if (this.user && this.user.isLogin) {
