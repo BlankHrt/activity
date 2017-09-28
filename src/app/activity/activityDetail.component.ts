6d0d31d3186b7bcee6428d0669b88843a627f087
liuqinghua <759345232@qq.com>
2017-09-28 09:24:02 +0800

    wx-server10-success1

--- src/app/activity/activityDetail.component.ts
+++ src/app/activity/activityDetail.component.ts
@@ -117,6 +117,54 @@ export class ActivityDetailComponent implements OnInit, AfterViewInit, OnDestroy
                 // 用户取消分享后执行的回调函数
               }
             });
+
+            wx.onMenuShareTimeline({
+              title: this.activity.title, // 分享标题
+              link: this.newUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+              imgUrl: this.imageList[0].medium, // 分享图标
+              success: function () {
+                // 用户确认分享后执行的回调函数
+                this.snackBar.open('分享成功');
+                setTimeout(() => {
+                  this.snackBar.dismiss();
+                }, 1500);
+              },
+              cancel: function () {
+                // 用户取消分享后执行的回调函数
+              }
+            });
+            wx.onMenuShareQQ({
+              title: this.activity.title, // 分享标题
+              desc: $('#summernote')[0].innerText, // 分享描述
+              link: this.newUrl, // 分享链接
+              imgUrl: this.imageList[0].medium, // 分享图标
+              success: function () {
+                // 用户确认分享后执行的回调函数
+                this.snackBar.open('分享成功');
+                setTimeout(() => {
+                  this.snackBar.dismiss();
+                }, 1500);
+              },
+              cancel: function () {
+                // 用户取消分享后执行的回调函数
+              }
+            });
+            wx.onMenuShareQZone({
+              title: this.activity.title, // 分享标题
+              desc: $('#summernote')[0].innerText, // 分享描述
+              link: this.newUrl, // 分享链接
+              imgUrl: this.imageList[0].medium, // 分享图标
+              success: function () {
+                // 用户确认分享后执行的回调函数
+                this.snackBar.open('分享成功');
+                setTimeout(() => {
+                  this.snackBar.dismiss();
+                }, 1500);
+              },
+              cancel: function () {
+                // 用户取消分享后执行的回调函数
+              }
+            });
           });
           this.meta.addTags([
             { name: 'keywords', content: this.activity.title },
@@ -143,7 +191,7 @@ export class ActivityDetailComponent implements OnInit, AfterViewInit, OnDestroy
           if (data.JsapiTicket) {
             this.newUrl = this.url + '/activity/activityDetail?id=' + params.id;
             this.activityService.signature(data.JsapiTicket, data.nonceStr, data.timestamp,
-              encodeURIComponent(this.newUrl)).subscribe(data2 => {
+              this.newUrl).subscribe(data2 => {
                 wx.config({
                   // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                   appId: 'wx3b6fe19df1feedfa', // 必填，公众号的唯一标识
@@ -161,72 +209,7 @@ export class ActivityDetailComponent implements OnInit, AfterViewInit, OnDestroy
         this.router.navigate(['/404']);
       }
     });
-    wx.onMenuShareAppMessage({
-      // title: 'this.activity.title', // 分享标题
-      title: '打篮球去', // 分享标题
-      desc: $('#summernote')[0].innerText, // 分享描述
-      link: this.newUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-      imgUrl: 'https://www.baidu.com/img/bd_logo1.png', // 分享图标
-      // imgUrl: this.imageList[0].medium, // 分享图标
-      type: '', // 分享类型,music、video或link，不填默认为link
-      dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-      success: function () {
-        this.snackBar.open('分享成功');
-        setTimeout(() => {
-          this.snackBar.dismiss();
-        }, 1500);
-      },
-      cancel: function () {
-        // 用户取消分享后执行的回调函数
-      }
-    });
-    wx.onMenuShareTimeline({
-      title: this.activity.title, // 分享标题
-      link: this.newUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-      imgUrl: this.imageList[0].medium, // 分享图标
-      success: function () {
-        // 用户确认分享后执行的回调函数
-        this.snackBar.open('分享成功');
-        setTimeout(() => {
-          this.snackBar.dismiss();
-        }, 1500);
-      },
-      cancel: function () {
-        // 用户取消分享后执行的回调函数
-      }
-    });
-    wx.onMenuShareQQ({
-      title: this.activity.title, // 分享标题
-      desc: $('#summernote')[0].innerText, // 分享描述
-      link: this.newUrl, // 分享链接
-      imgUrl: this.imageList[0].medium, // 分享图标
-      success: function () {
-        // 用户确认分享后执行的回调函数
-        this.snackBar.open('分享成功');
-        setTimeout(() => {
-          this.snackBar.dismiss();
-        }, 1500);
-      },
-      cancel: function () {
-        // 用户取消分享后执行的回调函数
-      }
-    });
-    wx.onMenuShareQZone({
-      title: this.activity.title, // 分享标题
-      desc: $('#summernote')[0].innerText, // 分享描述
-      link: this.newUrl, // 分享链接
-      imgUrl: this.imageList[0].medium, // 分享图标
-      success: function () {
-        // 用户确认分享后执行的回调函数
-        this.snackBar.open('分享成功');
-        setTimeout(() => {
-          this.snackBar.dismiss();
-        }, 1500);
-      },
-      cancel: function () {
-        // 用户取消分享后执行的回调函数
-      }
-    });
+
   }
 
   ngAfterViewInit(): void {
